f5153975071a1061ac1c69abedc58cf64bdc886b
Virat Patel <51950161+viratpatel1@users.noreply.github.com>
2021-04-17 18:28:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-// Create a Request instance
-var req = new XMLHttpRequest();
-//Initial the connection or create a connection
-req.open('GET','https://restcountries.eu/rest/v2/all',true);
-// Sendind the request
-req.send();
-// load the function
-//This function will bw triggered only when 
-req.onload = function(){
-    var data = JSON.parse(this.response);
-    for(var i in data){
-    console.log(i,data[i].name);
+var vp = new XMLHttpRequest();
+vp.open('GET' ,'https://restcountries.eu/rest/v2/all',true)
+vp.send();
+vp.onload = function(){
+    var country = JSON.parse(this.response);
+    for(var i in country){
+        try{
+            var cname = country[i].name;
+            var latlong = country[i].latlng;
+            if(latlong == 0){
+                ("Not Found")
+            }
+            weatherdata(cname,...latlong)
+        }
+        catch(e){
+            console.log("Invalid "+cname);
+        }
     }
 }
- 
+var weatherdata=function(name,lat,lng){
+    var URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=0b981b72e95bf4758a6b4680401e65b7`
+    var request = new XMLHttpRequest();
+    request.open('GET',URL,true);
+    request.send();
+    request.onload = function(){
+        var data = JSON.parse(this.response);
+        console.log(`${name} : ${data.main.temp}`);
+    }
+}
